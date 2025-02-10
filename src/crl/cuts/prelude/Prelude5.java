89be54a3cdc7218412965861e1f47440797e3eb3
Adam Crossan <adam.crossan@gmail.com>
2025-02-10 22:03:08 +0000

    Edited some mid-prologue text.
     - attempt at improving the awkward flow of Soleiyu (Prelude5) sentence
    with 'the former body of his father' etc.

--- src/crl/cuts/prelude/Prelude5.java
+++ src/crl/cuts/prelude/Prelude5.java
@@ -16,8 +16,24 @@ public class Prelude5 extends Unleasher {
 		Display.thus.showChat("PRELUDE_DRACULA4", game);
 		//level.getMonsterByID("PRELUDE_DRACULA").execute(ActionFactory.getActionFactory().getAction("PRELUDE_MORPH_SOLEIYU"));
 		//Display.thus.showChat(CharCuts.thus.get("PRELUDE_DRACULA4"));
-		Display.thus.showScreen("Soleiyu arrived to the dwelling of Dracula far too late... the former body of his father laying on the ground as the one last remain of the battle that had just finished, the dark count, almost unwounded, though his outfit burned and scratched, was at his throne having a sip of blood from his cup. Feeling the weight of his treason over his legacy, full of hatred against the murderer of his father, but more against himself for not following the path destiny had marked for him, he rushed against Dracula, whom with one alone raised hand and a slight gesture of dark manipulation, stopped the warrior and made him take again the shape that still lay deep inside him");
-		Display.thus.showScreen("Darkness swept the world, as all hopes seemed to be lost, and long forgotten Belmont bloodlines had to rise again and take their paths again to the cursed castle, the time has come for the future of mankind to be decided.");
+		Display.thus.showScreen(
+			"Soleiyu arrived far too late to Dracula's dwelling... His "+
+			"father's body lay fast upon the cold marble floor, in undeniable "+
+			"account of the battle just finished. The Count, seeming unwounded "+
+			"though his outfit was burnt and torn, reclined in his "+
+			"throne, sipping blood from an ornate glass.\nSoleiyu, feeling the weight of "+
+			"his treason over his legacy, full of hatred against the murderer "+
+			"of his father, but more against himself for not following the "+
+			"path destiny had marked for him, he rushed against Dracula, "+
+			"who with a lone raised hand and slight gesture of dark "+
+			"manipulation, stopped the warrior and made him take again the "+
+			"shape that still lay deep inside him");
+		
+		Display.thus.showScreen(
+			"Darkness swept the world, as all hopes seemed to be lost, and "+
+			"long forgotten Belmont bloodlines had to rise again and take "+
+			"their paths again to the cursed castle. The time has come for "+
+			"the future of mankind to be decided.");
 		game.exitGame();
 		//game.newGame();
 	}
